789524c594109de6cfa809c17ab5dbd1cf65b47d
luoqiaocong <luoqiaocong@gmail.com>
2026-08-18 15:10:40 +0800

    feat(user): 实现用户登录API接口和请求校验

--- app/modules/user/schemas.py
+++ app/modules/user/schemas.py
@@ -1,15 +1,57 @@
 from typing import Annotated
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, HttpUrl, field_serializer
 
+from .auth import get_hashed_id
+
+
+class UserIdentity(BaseModel):
+    id: Annotated[int, Field(description="用户ID")]
+    email: Annotated[EmailStr, Field(description="邮箱地址")]
 
 class UserRequest(BaseModel):
     email: Annotated[EmailStr, Field(description="邮箱地址")]
-    password: Annotated[str, Field(description="密码", min_length=8)]
-
+    password: Annotated[str, Field(description="用户密码", min_length=8)] 
 
 class RegisterUserRequest(UserRequest):
-    username: Annotated[str, Field(description="昵称", min_length=2, max_length=10)]
+    username: Annotated[str, Field(description="用户昵称", min_length=2, max_length=10)]
+    
+    model_config = {
+                "json_schema_extra": {
+                    "examples": [
+                        {
+                            "email": "admin@example.com",
+                            "password": "1234567890",
+                            "username": "admin"
+                        }
+                    ]
+                }
+            }
+    
     
 class LoginUserRequest(UserRequest):
-    pass
+    model_config = {
+                "json_schema_extra": {
+                    "examples": [
+                        {
+                            "email": "admin@example.com",
+                            "password": "1234567890"
+                        }
+                    ]
+                }
+            }
+
+
+
+class UserProfileBase(BaseModel):
+    username: Annotated[str | None, Field(description="昵称", max_length=10)] = None
+    avatar: Annotated[HttpUrl, Field(description="头像链接")]
+
+
+class UserInfo(UserIdentity, UserProfileBase):
+
+    @field_serializer('id')
+    def serialize_id(self, id: int, _info):
+        return get_hashed_id(id)
+
+    model_config = {"from_attributes": True}
