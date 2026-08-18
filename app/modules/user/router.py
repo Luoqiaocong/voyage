f15e8237527dcc72ccc892787fcbeeaa4d5b21f5
luoqiaocong <luoqiaocong@gmail.com>
2026-08-18 17:14:08 +0800

    feat(user): 添加获取用户信息接口（测试）

--- app/modules/user/router.py
+++ app/modules/user/router.py
@@ -2,14 +2,17 @@ from fastapi import APIRouter, Depends
 from fastapi_utils.cbv import cbv
 from starlette import status
 from app.core.route import UnifiedRoute
+from app.shared.db.models import User
 from .service import UserService
-from .schemas import RegisterUserRequest, LoginUserRequest
+from .schemas import RegisterUserRequest, LoginUserRequest, UserInfo
+from .dependencies import get_current_user
+
 router = APIRouter(prefix="/user", tags=["user"],route_class=UnifiedRoute)
 
 @cbv(router)
-class UserPublicRouter:
+class UserRouter:
     service : UserService = Depends()
-    
+   
     @router.post("/register", summary="用户注册", status_code=status.HTTP_201_CREATED)
     async def register(self, userdata: RegisterUserRequest):
         await self.service.to_register(userdata.email,userdata.password,userdata.username)
@@ -19,4 +22,10 @@ class UserPublicRouter:
         token = await self.service.to_login(userdata.email,userdata.password)
         return token
     
-
+    @router.get("/whoami", summary="获取当前用户信息", status_code=status.HTTP_200_OK,description="测试接口")
+    async def whoami(self,current_user: User = Depends(get_current_user)):
+            return UserInfo.model_validate(current_user)
+    
+    
+    
+    
