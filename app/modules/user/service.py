feec64821d512fda3aeb3f697e222eefd7a77cda
luoqiaocong <luoqiaocong@gmail.com>
2026-08-20 17:53:10 +0800

    feat(user): 修改密码增加新旧相同校验
    - to_change_pwd 增加 current_pwd == new_pwd 判断
    - 相同时抛出 USER_PWD_SAME (20011) 异常

--- app/modules/user/service.py
+++ app/modules/user/service.py
@@ -10,7 +10,12 @@ from app.shared.db import get_db
 from app.shared.db.models import User
 from app.shared.utils import TransactionMixin
 
-from .auth import PasswordManager, create_access_token, create_refresh_token, get_hashed_id
+from .auth import (
+    PasswordManager,
+    create_access_token,
+    create_refresh_token,
+    get_hashed_id,
+)
 from .repo import UserRepo
 
 
@@ -32,7 +37,6 @@ class UserService(TransactionMixin):
             raise UserException(code=BusinessCode.USER_NOT_FOUND)
         return user
 
-    # -------------------- 1. 注册 --------------------
     async def to_register(self, email: EmailStr, pwd: str, username: str):
         existing_user = await self.repo.get_user_dynamic(email=email)  # 先查数据库，确保邮箱唯一
         if existing_user:
@@ -41,7 +45,6 @@ class UserService(TransactionMixin):
         async with self.transaction_scope():
             return await self.repo.create(email, pwd, username)
 
-    # -------------------- 2. 登录 --------------------
     async def to_login(self, email: EmailStr, pwd: str):
         user = await self.repo.get_user_dynamic(email=email)
 
@@ -61,6 +64,28 @@ class UserService(TransactionMixin):
         user = await self._get_user(user_id=user_id)
         if not PasswordManager.verify(current_pwd, user.password):
             raise UserException(code=BusinessCode.USER_PWD_AUTH_FAILED)
+        if current_pwd == new_pwd:
+            raise UserException(code=BusinessCode.USER_PWD_SAME)
         new_pwd_hash  = PasswordManager.hash(new_pwd)
         async with self.transaction_scope():
-            await self.repo.change(new_pwd_hash,user)
+            await self.repo.modify(new_pwd_hash,user)
+            
+    async def to_change_profile(self, user_id: int, user_update_data: dict[str, str]):
+        user = await self._get_user(user_id=user_id) 
+        async with self.transaction_scope():
+            return await self.repo.update(user, user_update_data)
+               
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
